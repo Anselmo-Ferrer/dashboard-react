562b2e0792ff3cd01fb14a159f76b833df32adbb
Anselmo Dev <Anselmodev@MacBook-Pro-de-Anselmo.local>
2024-08-02 18:40:43 -0300

    authentication page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import SellersPage from './components/sellers-page/sellers';
 import NotFoundPage from './components/not-found-page/notfound-page';
 import SettingsPage from './components/settings-pages/settings';
 import AdministrationPage from './components/admins-pages/administration';
+import RegisterPage from './components/authentication/register';
+import LoginPage from './components/authentication/login';
 
 export default function page() {
   return (
@@ -15,6 +17,8 @@ export default function page() {
         <Route path="/vendas" element={<SellersPage/>}/>
         <Route path="/gerenciamento" element={<AdministrationPage/>}/>
         <Route path="/ajustes" element={<SettingsPage/>}/>
+        <Route path="/register" element={<RegisterPage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
         <Route path="*" element={<NotFoundPage/>}/>
       </Routes>
     </BrowserRouter>
