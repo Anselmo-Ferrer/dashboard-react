09401bc7483e2c36f2aa3c2cd5075c794a12b9b5
Anselmo Dev <Anselmodev@MacBook-Pro-de-Anselmo.local>
2024-08-01 21:28:16 -0300

    analytics page

--- src/components/overview-pages/analytics.tsx
+++ src/components/overview-pages/analytics.tsx
@@ -1,11 +1,25 @@
 
 
 import { TabsContent } from "@radix-ui/react-tabs"
+import { RadialGraph } from "../charts/radial-graph"
+import { AreaGraph2 } from "../charts/area-graph-2"
+import { LineGraph } from "../charts/line-graph"
+import { BarGraph2 } from "../charts/bar-graph-2"
 
 function AnalyticsPage() {
   return (
     <TabsContent value="analytics" className="space-y-4">
-            <h1>hello world!</h1>
+        <div className="w-full flex gap-5">
+          <RadialGraph/>
+          <RadialGraph/>
+          <RadialGraph/>
+          <RadialGraph/>
+        </div>
+          <AreaGraph2 />
+        <div className="w-full flex gap-5">
+          <LineGraph />
+          <BarGraph2 />
+        </div>
     </TabsContent>
   )
 }
